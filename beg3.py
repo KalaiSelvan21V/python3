3f4abc7ab9642220be8b16ea84ee2eb8bd523be2
KalaiSelvan21V <51901987+KalaiSelvan21V@users.noreply.github.com>
2019-06-17 15:42:32 +0530

    Create beg3.py

---
+++ beg3.py
@@ -0,0 +1,9 @@
+o=input()
+vowel=['a','A','e','E','i','I','o','O','u','U']
+Consonant=['b','B','c','C','d','D','f','F','j','J','k','K','l','L','m','M','n','N','p','P','q','Q','r','R','s','S','t','T','v','V','w','W','x','X','y','Y','z','Z']
+if o in vowel:
+    print("Vowel")
+elif o in Consonant:
+    print("Consonant")
+else:
+    print("invalid")
